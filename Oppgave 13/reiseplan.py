ca17c49cecf5d2e9e62acd680f2e19db16570340
GulliverWestbye <gulliver.westbye@icloud.com>
2022-08-30 13:22:50 +0200

    commit

--- Oppgave 13/reiseplan.py
+++ Oppgave 13/reiseplan.py
@@ -1,10 +1,34 @@
 steder = []
+klesplagg = []
+avreisedatoer = []
 
 for i in range(5):
-    reisemål = input("Skriv inn dine reisemål: ")
-    
-    
+    reisemål = input("Skriv inn dine fem reisemål: ")
+    steder.append(reisemål)
 
-print(steder)
+for i in range(5):
+    klær = input("Skriv inn dine fem plagg: ")
+    klesplagg.append(klær)
 
-#ikke ferdig
+for i in range(5):
+    reiser = input("Skriv inn dine fem avreisedatoer: ")
+    avreisedatoer.append(reiser)
+
+
+reiseplan = [steder, klesplagg, avreisedatoer]
+
+for i in reiseplan:
+    print(i)
+
+
+i1 = int(input("Skriv inn plass: "))
+
+i2 = int(input("Skriv inn plass i plass: "))
+
+
+if i1 in range(-5, 5) and i2 in range(-5, 5):
+    print(reiseplan[i1][i2])
+
+else:
+    print("Ugyldig input")
+
